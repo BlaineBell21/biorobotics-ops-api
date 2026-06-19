b0c178187ce0e2564a1d1aa5a3dc6f1f603f7327
Blaine Bell <blainebell21@gmail.com>
2026-06-19 10:21:51 -0700

    Initial commit

---
+++ src/main/java/com/bunbun/biorobotics_ops_api/BioroboticsOpsApiApplication.java
@@ -0,0 +1,13 @@
+package com.bunbun.biorobotics_ops_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioroboticsOpsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioroboticsOpsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bunbun/biorobotics_ops_api/BioroboticsOpsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunbun.biorobotics_ops_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioroboticsOpsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
