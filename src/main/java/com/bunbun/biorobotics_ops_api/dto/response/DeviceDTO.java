05bbbe4aaa798068cd6f16027bda4654826d1425
Blaine Bell <blainebell21@gmail.com>
2026-08-03 11:47:04 -0700

    device DTO feature branch

--- src/main/java/com/bunbun/biorobotics_ops_api/dto/response/DeviceDTO.java
+++ src/main/java/com/bunbun/biorobotics_ops_api/dto/response/DeviceDTO.java
@@ -1,4 +1,16 @@
 package com.bunbun.biorobotics_ops_api.dto.response;
 
-public class DeviceDTO {
+import com.bunbun.biorobotics_ops_api.model.enums.DeviceStatus;
+
+
+
+public record DeviceDTO(
+   Long id,
+   String deviceCode,
+   String name,
+   String modelNumber,
+   String manufacturer,
+   String serialNumber,
+   DeviceStatus status
+) {
 }
